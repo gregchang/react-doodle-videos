d1bcfe812faf0cc65dad6512bb9cb9229cd5f47f
Greg Chang <gregchangio@gmail.com>
2019-03-22 23:56:16 -0500

    Add yotube.js without api key

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'INSERT_YOUTUBE_DATA_API_KEY';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
+});
